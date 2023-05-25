8d16f74ffdfe8e8ce7f97b38674a502640e9518d
jonander-upm <jonander.gainzarain@alumnos.upm.es>
2023-05-25 19:19:13 +0200

    Added CORS Mapping and included used data in JWT jonander-upm/web-laiacano#2

--- src/main/java/com/laiacano/core/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/laiacano/core/config/jwt/JwtTokenProvider.java
@@ -1,19 +1,17 @@
 package com.laiacano.core.config.jwt;
 
-import java.util.Base64;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
+import com.laiacano.core.data.entities.User;
 import com.laiacano.core.services.UserDetailsService;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
@@ -21,6 +19,10 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtTokenProvider {
+    private static final String USERNAME_CLAIM = "username";
+    private static final String EMAIL_CLAIM = "email";
+    private static final String ROLE_CLAIM = "role";
+
     @Value("${app.jwt.secret")
     private String secretKey;
     @Value("${app.jwt.validity}")
@@ -35,13 +37,14 @@ public class JwtTokenProvider {
     }
 
     public String createToken(User user) {
-        String username = user.getUsername();
-        Claims claims = Jwts.claims().setSubject(username);
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
-
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(USERNAME_CLAIM, user.getUsername());
+        claims.put(EMAIL_CLAIM, user.getEmail());
+        claims.put(ROLE_CLAIM, user.getRole());
         return Jwts.builder()
-                .setClaims(claims)
+                .addClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
                 .signWith(SignatureAlgorithm.HS256, secretKey)
@@ -52,9 +55,9 @@ public class JwtTokenProvider {
         Claims claims = Jwts.parser().setSigningKey(secretKey)
                 .parseClaimsJws(token)
                 .getBody();
-        User principal = new User(claims.getSubject(), "", List.of());
-
-        return new UsernamePasswordAuthenticationToken(principal, token, null);
+        String username = claims.get(USERNAME_CLAIM, String.class);
+        return new UsernamePasswordAuthenticationToken(this.userDetailsService.findByUsername(username),
+                token, null);
     }
 
     public String getUsername(String token) {
